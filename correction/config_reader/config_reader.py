b9332eb0e8cc2ac2554159655dc13dc675401fd0
mateuslatrova <mateuslatrova@usp.br>
2023-10-27 16:57:45 -0300

    turn "get_dataset_path" method from ConfigReader a public method

--- correction/config_reader/config_reader.py
+++ correction/config_reader/config_reader.py
@@ -18,28 +18,28 @@ class ConfigReader:
         return self.config[key]
 
     def get_train_raw_dataset_path(self):
-        return self._get_dataset_path(
+        return self.get_dataset_path(
             self.raw_files_dirname, self.get("raw_train_filename"), self.get("raw_files_extension")
         )
 
     def get_test_raw_dataset_path(self):
-        return self._get_dataset_path(
+        return self.get_dataset_path(
             self.raw_files_dirname, self.get("raw_test_filename"), self.get("raw_files_extension")
         )
 
     def get_preprocessed_dataset_path(self):
-        return self._get_dataset_path(
+        return self.get_dataset_path(
             self.preprocessed_files_dirname,
             self.get("preprocessed_dirname"),
         )
 
     def get_processed_dataset_path(self):
-        return self._get_dataset_path(
+        return self.get_dataset_path(
             self.processed_files_dirname,
             self.get("processed_dirname"),
         )
 
-    def _get_dataset_path(self, level, filename, file_extension=None):
+    def get_dataset_path(self, level, filename, file_extension=None):
         if file_extension is not None:
             filename = f"{filename}.{file_extension}"
         return os.path.join(self.base_dirpath, self.data_dirname, level, filename)
